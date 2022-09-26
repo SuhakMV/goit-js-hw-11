b3ae79427df68bac5d87f3ca3849a45b87930dad
SuhakMV <103995334+SuhakMV@users.noreply.github.com>
2022-09-26 16:37:53 +0000

    Deploying to gh-pages from @ SuhakMV/goit-js-hw-11@34a93e14fe496d37b3d83e3447e234e19a8efb36 🚀

--- index.027e3e8f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={searchForm:document.querySelector(".search-form"),gallery:document.querySelector(".gallery"),submitBtn:document.querySelector(".submit"),loadMoreBtn:document.querySelector(".load-more")};e.searchForm.addEventListener("submit",(function(e){e.preventDefault()})),e.loadMoreBtn.addEventListener("click",(function(){}))}();
-//# sourceMappingURL=index.027e3e8f.js.map
