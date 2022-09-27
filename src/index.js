55c688b5fdbc7896e6a7389bad641f43c28e7ead
SuhakMV <sugamish@gmail.com>
2022-09-27 12:06:08 +0300

    27091206

--- src/index.js
+++ src/index.js
@@ -16,11 +16,10 @@ refs.loadMoreBtn.addEventListener('click', onLoadMore);
 function onSearch (e) {
     e.preventDefault();
 
+    clearCardContainer();
     newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
     newsApiService.resetPage();
-    newsApiService.fetchArticles().then (hits => {
-        console.log(hits);
-    });
+    newsApiService.fetchArticles().then (appendMarkupCards);
 }
 
 
@@ -28,4 +27,45 @@ function onLoadMore () {
     newsApiService.fetchArticles().then (hits => {
         console.log(hits);
     });
+}
+
+function clearCardContainer() {
+    refs.gallery.innerHTML = '';
+}
+
+function appendMarkupCards(cards) {
+    refs.gallery.insertAdjacentHTML('beforeend', markupCards(cards));
+}
+
+function markupCards(hits) {
+
+    return hits.map(({ webformatURL,
+        largeImageURL,
+        views,
+        likes,
+        comments,
+        downloads }) => {
+            return `
+            <div class="photo-card">
+                <a class="photo-card__link" href="${largeImageURL}">
+                    <img class="photo-card__img" src="${webformatURL}" alt="" loading="lazy"/>
+                </a>
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>${likes}
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>${views}
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>${comments}
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>${downloads}
+                    </p>
+                </div>
+            </div>
+            `;
+        })
+        .join('');
 }
